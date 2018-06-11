524409cf7304c11a449b28a8e5f7b3e96d87a53d
Isaac <sc15i2gp@leeds.ac.uk>
2018-06-11 21:22:21 +0100

    Opens files which exist. Rejects ones that don't.

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
+//I want to specify a file name and the program should read it:
+//Outputting its width and height
+//	Open file with given file name
+
+typedef unsigned char byte;
+
+byte processFile(const char* filePath)
+{
+	FILE* file = fopen(filePath, "r");
+	if(!file)
+	{
+		printf("Error: Could not open %s!\n", filePath);
+		return -1;
+	}
+	
+	//Read file here
+
+	fclose(file);
+	return 0;
+}
+
 int main(int argc, char** argv)
 {
-	printf("Hello World!\n");
-	return 0;
+	if(argc <= 1)
+	{
+		printf("Error: No file specified!\n");
+		return 1;
+	}
+	else
+	{
+		return processFile(argv[1]);
+	}
 }
