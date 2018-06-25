859ac86e109907f5ed805418615a5ca9fb5b8589
Isaac <sc15i2gp@leeds.ac.uk>
2018-06-25 18:41:26 +0100

    Symbol list loaded.

--- main.c
+++ main.c
@@ -6,11 +6,62 @@
 #include "Map.h"
 
 //TODO: Create pattern image from input ppm
-//	Load pattern symbols into list
-//	Create array of pattern symbol hashes with width*height of input file
-//	Hashmap of colour to times colour occurs
-//	Fill pattern symbol hashes array with hashes corresponding to each pixel's colour in input file
-//	Convert pattern symbol hashes array to uint array to output to ppm file
+//	Load list of pattern symbols (list of file_data*)
+//	
+//	Create colour map
+//	For each pixel in input file:
+//		If pixel's colour hash is not in colour map
+//			Get next pattern symbol not in use
+//			Set to in use
+//			Make pixel's colour hash map to pattern symbol
+//
+//	Create file_data* for output pattern
+//	Calculate new size of file_data* colour_vals* and allocate
+//	For each row of pixels in input file:
+//		For each row of pixels in pattern symbol (i = 0 to 15):
+//			For each pixel in a row of input ppm
+//				Copy row[i] pixel values of pattern` symbol
+//
+
+const char* symbol_prefix = "Symbols/S_";
+const char* symbol_extension = ".ppm";
+
+#define NUM_SYMBOLS 31
+
+struct pattern_symbol_list
+{
+	struct file_info* symbol_files[NUM_SYMBOLS];
+	byte is_symbol_in_use[NUM_SYMBOLS];
+};
+
+void load_symbol(const char* symbol_path, struct file_info** symbol_loc)
+{
+	*symbol_loc = process_file(symbol_path);
+}
+
+struct file_info** load_symbols()
+{
+	struct file_info** symbols = new struct file_info*[NUM_SYMBOLS];
+	
+	for(uint i = 0; i < NUM_SYMBOLS; i++)
+	{
+		char* path = new char[strlen(symbol_prefix) + strlen(symbol_extension) + 2];
+		sprintf(path, "%s%02d%s", symbol_prefix, i, symbol_extension);
+		symbols[i] = process_file(path);
+		delete[] path;
+	}
+
+	return symbols;
+}
+
+void destroy_symbols(struct file_info** symbols)
+{
+	for(uint i = 0; i < NUM_SYMBOLS; i++)
+	{
+		destroy_file(symbols[i]);
+	}
+	delete[] symbols;
+}
 
 void print_colour_pair(struct kv_pair* p, uint colour)
 {
@@ -22,6 +73,7 @@ void print_colour_map(struct rb_tree* t)
 {
 	printf("===========================================\n");
 	execute_for_each_pair(t, print_colour_pair);
+	printf("Number of colours = %d\n", count_nodes(t));
 	printf("===========================================\n\n");
 }
 void assert_node(struct node* n, uint key, uint colour)
@@ -107,8 +159,23 @@ void count_colours_in_file(struct file_info* file_data)
 	destroy_rb_tree(colour_map);
 }
 
+void test_symbols()
+{
+	struct file_info** symbols = load_symbols();
+
+	for(uint i = 0; i < NUM_SYMBOLS; i++)
+	{
+		char* path = new char[2 + 4];
+		sprintf(path, "%02d%s", i, ".ppm");
+		print_to_ppm(path, symbols[i]);
+	}
+
+	destroy_symbols(symbols);
+}
+
 int main(int argc, char** argv)
 {
+	test_symbols();
 	if(argc <= 1)
 	{
 		printf("Error: No file specified!\n");
