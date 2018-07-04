662afb32c817ae6c2870dc3dd8db5482af0eb73a
Isaac <sc15i2gp@leeds.ac.uk>
2018-07-04 23:26:07 +0100

    Changed TODO and gitignore.

--- main.c
+++ main.c
@@ -6,23 +6,6 @@
 
 //END RESULT: Program takes an image, creates a ppm by interpolating areas of colour, outputs a cross stitch pattern from this
 
-//NOTE: Segfault on i = 44 when loading DMC floss map, due to being unable to find pair in get_pair()
-
-//TODO: Fix above segfault:
-//	Issue is due to erroneous pointer setting (parent ptrs pointing to grandparent etc.)
-//	Make standard function set_child(node n, node c, bool left) which not only sets n->child but also sets c->parent
-
-
-//TODO: Make load_symbols place white first and black second in symbols_list
-
-//TODO: Make rb_tree more generic
-
-//TODO: web scrape DMC floss code list and place into .c file
-
-//TODO: Map of colour => floss code
-
-//TODO: Map of symbol index => floss code
-
 //TODO: Floss codes
 //	Floss codes are DMC
 //	Already have map of colours => symbols
@@ -33,6 +16,8 @@
 //		Print symbol => floss pair
 //
 //	Include symbol to DMC list on pattern
+
+//TODO: Put symbols => flosses map on output pattern
 
 
 int main(int argc, char** argv)
